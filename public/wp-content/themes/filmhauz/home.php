19f8c0220554d97a2b58b45b1fb3de6c2c8132b0
jcamou <jccamou@gmail.com>
2016-02-22 07:37:21 -0600

    local changes

---
+++ public/wp-content/themes/filmhauz/home.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+
+	<h3><?php the_title(); ?></h3>
+	<?php the_content(); ?>
+
+<?php endwhile; else: ?>
+
+	<p>There are no pages here</p>
+	
+<?php endif; ?>
+
+<p>This is home.php</p>
+
+<?php get_footer(); ?>
